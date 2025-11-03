e73b698be182220f16d4a6cd2e8d7ac3f382d6d4
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-11-02 19:07:36 -0500

    Added login endpoint for the js-service-api

--- packages/js-service-api/server.js
+++ packages/js-service-api/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const PORT = 5000;
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 const app = express();
+
 
 app.use(express.json());
 
 app.get('/ping', (req, res) => {
     res.json({message: "pong"});
 });
+
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+
+    if(username === process.env.USERNAME && password === process.env.PASSWORD){
+        res.json({message: "login successful"});
+    }
+    else{
+        res.status(401).json({message: "Invalid username or password"});
+    }
+})
 
 app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`));
